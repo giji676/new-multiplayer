5b21cdbb060d5cd14c17472184ee0e4cd46fdfbd
giji676 <tvtengoxachidze@gmail.com>
2022-01-18 23:22:58 +0000

    Initial commit

---
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerControl : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+
+    private PlayerMotor motor;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        //Calculate movement velocity as a 3D vector
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+
+    }
+
+}
