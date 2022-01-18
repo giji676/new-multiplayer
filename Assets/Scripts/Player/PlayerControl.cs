68bb1900d459581557c144f29c93c9d78a2a8687
giji676 <tvtengoxachidze@gmail.com>
2022-01-18 23:45:13 +0000

    added basic movement

--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -1,10 +1,19 @@
+using Unity.Netcode;
 using UnityEngine;
 
-[RequireComponent(typeof(PlayerMotor))]
-public class PlayerControl : MonoBehaviour
+public class PlayerControl : NetworkBehaviour
 {
     [SerializeField]
     private float speed = 5f;
+
+    [SerializeField]
+    private float sensitivity = 1.5f;
+
+    [SerializeField]
+    private Vector2 defaultInitialPalnePosition = new Vector2(-4, 4);
+
+    private float _xMov;
+    private float _zMov;
 
     private PlayerMotor motor;
 
@@ -15,8 +24,26 @@ public class PlayerControl : MonoBehaviour
 
     private void Update()
     {
-        //Calculate movement velocity as a 3D vector
-        float _xMov = Input.GetAxisRaw("Horizontal");
-        float _zMov = Input.GetAxisRaw("Vertical");
+        // Player rotation and position input
+        // Calculate movement velocity as a 3D vector
+        _xMov = Input.GetAxisRaw("Horizontal");
+        _zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _moveHorizontal = transform.right * _xMov;
+        Vector3 _moveVertical = transform.forward * _zMov;
+
+        // Final movement vector
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * speed;
+
+        // Calcualte rotation as a 3D vector (turning around)
+        float _yRot = Input.GetAxisRaw("Mouse X");
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * sensitivity;
+
+        // Calcualte camera rotation as a 3D vector
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+        Vector3 _cameraRotation = new Vector3(_xRot, 0f, 0f) * sensitivity;
+
+        // Apply _velocity, _rotation, _cameraRotation in PlayerMotor
+        motor.UpdateClientVelocityRotation(_velocity, _rotation, _cameraRotation);
     }
 }
--- Assets/Scripts/Player/PlayerMotor.cs
+++ Assets/Scripts/Player/PlayerMotor.cs
@@ -1,6 +1,69 @@
 using UnityEngine;
+using Unity.Netcode;
+
 
 [RequireComponent(typeof(Rigidbody))]
-public class PlayerMotor : MonoBehaviour
+public class PlayerMotor : NetworkBehaviour
 {
+    [SerializeField]
+    private Camera cam;
+
+    [SerializeField]
+    private NetworkVariable<Vector3> networkVelocity = new NetworkVariable<Vector3>();
+
+    [SerializeField]
+    private NetworkVariable<Vector3> networkRotation = new NetworkVariable<Vector3>();
+
+    [SerializeField]
+    private NetworkVariable<Vector3> networkCameraRotation = new NetworkVariable<Vector3>();
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update local variables from PlayerControl
+    public void UpdateClientVelocityRotation(Vector3 _velocity, Vector3 _rotation, Vector3 _cameraRotation)
+    {
+        if (networkVelocity.Value != _velocity || networkRotation.Value != _rotation || networkCameraRotation.Value != _cameraRotation)
+        {
+            UpdateClientPosRotServerRpc(_velocity, _rotation, _cameraRotation);
+        }
+    }
+
+    // Run every physics iteration
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        perfromRotation();
+    }
+
+    // Perform movement based on velocity variable
+    private void PerformMovement()
+    {
+        if (networkVelocity.Value != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + networkVelocity.Value * Time.deltaTime);
+        }
+    }
+
+    // Perform rotation
+    private void perfromRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(networkRotation.Value));
+        if (cam != null)
+        {
+            cam.transform.Rotate(-networkCameraRotation.Value);
+        }
+    }
+
+    [ServerRpc]
+    public void UpdateClientPosRotServerRpc(Vector3 newVelocity, Vector3 newRotation, Vector3 newCameraRotation)
+    {
+        networkVelocity.Value = newVelocity;
+        networkRotation.Value = newRotation;
+        networkCameraRotation.Value = newCameraRotation;
+    }
 }
