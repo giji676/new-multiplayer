97fd1f54bf8d0523be9b42d239bf49e18a4dda2e
giji676 <tvtengoxachidze@gmail.com>
2022-01-19 11:03:26 +0000

    Hooked the start buttons to the code

---
+++ Assets/Scripts/Game/UIManager.cs
@@ -0,0 +1,47 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Unity.Netcode;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Button startHostButton;
+
+    [SerializeField]
+    private Button startClientButton;
+
+    [SerializeField]
+    private Button startServerButton;
+
+    [SerializeField]
+    private TextMeshProUGUI playersInGameText;
+
+    private void Awake()
+    {
+        Cursor.visible = true;
+    }
+
+    private void Update()
+    {
+        //playersInGameText.text = $"Players in game: {PlayerManager.Instance.PlayersInGame}";
+    }
+
+    private void Start()
+    {
+        startHostButton.onClick.AddListener(() => 
+        {
+            NetworkManager.Singleton.StartHost();
+        });
+
+        startClientButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartClient();
+        });
+
+        startServerButton.onClick.AddListener(() =>
+        {
+            NetworkManager.Singleton.StartServer();
+        });
+    }
+}
