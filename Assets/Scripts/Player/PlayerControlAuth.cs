94355e9d0e6b439cebb211c70e5fbd979a28b614
giji676 <tvtengoxachidze@gmail.com>
2022-01-22 17:50:12 +0000

    Running code and animation

--- Assets/Scripts/Player/PlayerControlAuth.cs
+++ Assets/Scripts/Player/PlayerControlAuth.cs
@@ -7,11 +7,19 @@ public class PlayerControlAuth : NetworkBehaviour
     {
         Idle,
         Walk,
-        ReverseWalk
+        Run,
+        ReverseWalk,
+        ReverseRun
     }
 
     [SerializeField]
-    private float speed = 5f;
+    private float walkSpeed = 5f;
+
+    [SerializeField]
+    private float runSpeed = 10f;
+
+    [SerializeField]
+    private float currentSpeed = 0f;
 
     [SerializeField]
     private float sensitivity = 3f;
@@ -23,7 +31,8 @@ public class PlayerControlAuth : NetworkBehaviour
     private NetworkVariable<PlayerState> networkPlayerState = new NetworkVariable<PlayerState>();
 
     private float _xMov;
-    private float _zMov; 
+    private float _zMov;
+    private bool _run;
 
     private PlayerMotorAuth motor;
     private Animator animator;
@@ -59,11 +68,22 @@ public class PlayerControlAuth : NetworkBehaviour
         _xMov = Input.GetAxisRaw("Horizontal");
         _zMov = Input.GetAxisRaw("Vertical");
 
+        _run = Input.GetKey(KeyCode.LeftShift);
+
         Vector3 _moveHorizontal = transform.right * _xMov;
         Vector3 _moveVertical = transform.forward * _zMov;
 
+        if (_run)
+        {
+            currentSpeed = runSpeed;
+        }
+        else
+        {
+            currentSpeed = walkSpeed;
+        }
+
         // Final movement vector
-        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * speed;
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * currentSpeed;
 
         // Calcualte rotation as a 3D vector (turning around)
         float _yRot = Input.GetAxisRaw("Mouse X");
@@ -87,13 +107,21 @@ public class PlayerControlAuth : NetworkBehaviour
     private void PlayerStateUpdate()
     {
         // Player state changes
-        if (_zMov > 0)
+        if (_zMov > 0 && !_run)
         {
             UpdatePlayerStateServerRpc(PlayerState.Walk);
         }
-        else if (_zMov < 0)
+        else if (_zMov > 0 && _run)
+        {
+            UpdatePlayerStateServerRpc(PlayerState.Run);
+        }
+        else if (_zMov < 0 && !_run)
         {
             UpdatePlayerStateServerRpc(PlayerState.ReverseWalk);
+        }
+        else if (_zMov < 0 && _run)
+        {
+            UpdatePlayerStateServerRpc(PlayerState.ReverseRun);
         }
         else
         {
@@ -108,9 +136,17 @@ public class PlayerControlAuth : NetworkBehaviour
         {
             animator.SetFloat("Walk", 1);
         }
+        else if (networkPlayerState.Value == PlayerState.Run)
+        {
+            animator.SetFloat("Walk", 2);
+        }
         else if (networkPlayerState.Value == PlayerState.ReverseWalk)
         {
             animator.SetFloat("Walk", -1);
+        }
+        else if (networkPlayerState.Value == PlayerState.ReverseRun)
+        {
+            animator.SetFloat("Walk", -2);
         }
         else
         {
